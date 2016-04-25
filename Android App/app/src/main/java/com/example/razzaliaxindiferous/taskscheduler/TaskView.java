10e6f235036a7c1d18fa1481605633608862a4cf
mjgood <mjgood91@gmail.com>
2016-04-25 18:49:43 -0400

    Task Display now displays details of selected task
    * Task Display now displays details of selected task.

--- Android App/app/src/main/java/com/example/razzaliaxindiferous/taskscheduler/TaskView.java
+++ Android App/app/src/main/java/com/example/razzaliaxindiferous/taskscheduler/TaskView.java
@@ -6,13 +6,22 @@
 
 package com.example.razzaliaxindiferous.taskscheduler;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class TaskView extends AppCompatActivity {
 
@@ -36,6 +45,31 @@ public class TaskView extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        ContentResolver cr = getContentResolver();
+        String[] values = {
+                "_id",
+                "subject",
+                "description",
+                "priority",
+                "end_time"};
+
+        String[] results = new String[5];
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        Log.d ("Task Selected:", Long.toString(taskSelected));
+        String[] selectionArgs = {
+                Long.toString(taskSelected)};
+
+        Cursor c = cr.query(DailyTaskContentProvider.CONTENT_URI, values, "_id=?", selectionArgs, null);
+
+        if (c.moveToFirst()) {
+            ((TextView) findViewById(R.id.textViewSubject)).setText(c.getString(1));
+            ((TextView) findViewById(R.id.textViewDescription)).setText(c.getString(2));
+            ((TextView) findViewById(R.id.textViewPriority)).setText(c.getString(3));
+            ((TextView) findViewById(R.id.textViewEndDate)).setText(c.getString(4));
+        }
     }
 
     //######################################################################
